77529819cd3f35719c1dfa524b901c8d56a89d50
Emanuel Barreto <ebarreto@invico.gov.ar>
2023-04-24 09:16:09 -0300

    funcionalidad base calcu

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
-const operador = [ "*", "/", "+", "-",];
-const especial = [ "reset", "del", "=", "."];
+const especiales = ["reset", "del"];
 var screen = document.querySelector(".calc__screen p");
 var btns = document.querySelectorAll(".btns button"); //Este me trae todas las etiquetas button de la seccion btns
-var value1 = "";  //Primer valor ingresado
-var value2 = "";  //Segundo valor ingresado
-var value = "";   //Valor despues de una operacion
 
-btns.forEach( btn => {  //Con el forEach puedo recorrer todas esas etiquetas y aplicarle el dataset para saber que valor tiene el boton apretado
-    btn.onclick = function() {
-        //----------------------------
-        //Valores para funcionamiento
+btns.forEach(btn => { //Con el forEach puedo recorrer todas esas etiquetas y aplicarle el dataset para saber que valor tiene el boton apretado
+    btn.onclick = function () {
+
         let valor = btn.dataset.btn; //Almacenamos el valor del boton clickeado
-       
 
-        //----------------------------
-        if(operador.includes(valor)){ //Es un operador?
-          
-        } 
-        
-        else if (especial.includes(valor)){//Es un especial?
-            
-        } 
-        
-        else { //Es un numero
-            
+
+        if (especiales.includes(valor)) {
+            if (valor === "reset") {
+                screen.textContent = "";
+            } else if (valor === "del") {
+                screen.textContent = screen.textContent.slice(0, -1)
+            }
+        } else {
+            if (valor !== "=") {
+                screen.append(valor);
+            } else {                
+                screen.textContent = eval(screen.textContent);
+            }
         }
     }
-}
-)
+})
