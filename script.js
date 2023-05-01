8dd4ab5eb2dae3a4f8cc71dd606c1739f7c91b57
Emmanuel Barreto <barretosantiago@hotmail.com>
2023-05-01 16:23:40 -0300

    definitivo

--- script.js
+++ script.js
@@ -1,81 +1,101 @@
-const operacion = ["*", "+", "-", "/", "."];
-let screen = document.querySelector(".calc__screen p");
-let btns = document.querySelectorAll(".btns button"); //Este me trae todas las etiquetas button de la seccion btns
+ const operacion = ["*", "+", "-", "/"];
+ let screen = document.querySelector(".calc__screen p");
+ let btns = document.querySelectorAll(".btns button"); //Este me trae todas las etiquetas button de la seccion btn
+ let str = "";
 
-let str = "";
+ //-----------------------------------------------------
+ function Delete() {
+     screen.textContent = screen.textContent.slice(0, -1);
+     str = str.slice(0, -1)
 
-//-----------------------------------------------------
-function Delete() {
-    screen.textContent = screen.textContent.slice(0, -1)
-}
+ }
 
-function Reset() {
-    screen.textContent = "";
-}
+ function Reset() {
+     screen.textContent = "";
+ }
 
-function Agregar(value) {
-    str += value;
-    screen.append(value);
-}
-
-function Resultado(value) {
-    if (eval(value).toString().length < 16) {
-        screen.textContent = eval(value);
-    } else {
-        screen.textContent = eval(value).toExponential(5)
-    }
-    str = screen.textContent;
-}
+ function Agregar(value) {
+     str += value;
+     screen.textContent += value;
 
 
-//----------------------------------------------------------
+ }
 
-btns.forEach(btn => { //Con el forEach puedo recorrer todas esas etiquetas y aplicarle el dataset para saber que valor tiene el boton apretado
-    btn.onclick = function () {
+ function Resultado(value) {
+     if (eval(value).toString() === "Infinity") {
+         screen.textContent = "∞";
+         setTimeout(() => {
+             screen.textContent = "";
+             str = "";
+         }, 2000)
+     } else if (eval(value).toString() === "NaN") {
+         screen.textContent = "Indeterminado";
+         setTimeout(() => {
+             screen.textContent = "";
+             str = "";
+         }, 2000)
+     } else {
+         if (eval(value).toString().length < 16) {
+             screen.textContent = eval(value);
+             str = eval(value);
+         } else {
+             screen.textContent = eval(value).toExponential(5)
+         }
+     }
+ }
 
-        let input = btn.dataset.btn; //Almacenamos el valor del boton clickeado
+ //----------------------------------------------------------
 
-        // 
-        switch (input) {
-            case "del":
-                Delete();
-                break;
-            case "reset":
-                Reset();
-                str = "";
-                break;
-            case "=":
-                console.log(str);
-                if (screen.textContent.length !== 0) {//Evito que ingrese un str vacio a la fn
-                    if (!operacion.includes(str.slice(-1))) { //Evito que ingrese a la funcion un str donde el ultimo parametro sea un operador
-                        Resultado(str);
-                    }
-                }
+ btns.forEach(btn => { //Con el forEach puedo recorrer todas esas etiquetas y aplicarle el dataset para saber que valor tiene el boton apretado
+     btn.onclick = function () {
 
-                break;
-            default:
+         let input = btn.dataset.btn; //Almacenamos el valor del boton clickeado
 
-                if (operacion.includes(input)) { //Si es una simbolo de operacion
-                    if (!str.includes(input)) {
-                        if (screen.textContent.length !== 0) {
-                            str += input;
-                            Reset();
-                        }
-                    }
-                } else {
-                    if (screen.textContent == "0") { //si en el screen ya se ingreso un 0
-                        if (input === ".") { //si el valor a agregar es un punto
-                            Agregar(input)
-                        }
-                    } else {
-                        if (screen.textContent.length < 16) {
-                            Agregar(input)
-                        } else {
-                            screen.textContent = Number(screen.textContent).toExponential(5);
-                        }
-                    }
-                }
-                break
-        }
-    }
-})
+         // 
+         if (screen.textContent.length > 16) {
+             screen.textContent = Number(screen.textContent).toExponential(5);
+         }
+
+         switch (input) {
+             case "del":
+                 if (screen.textContent !== "") {
+                     Delete();
+                 }
+                 break;
+             case "reset":
+                 Reset();
+                 str = "";
+                 break;
+             case "=":
+                 if (screen.textContent.length !== 0) { //Evito que ingrese un str vacio a la fn
+                     if (!operacion.includes(str[str.length - 1])) { //Evito que ingrese a la r
+                         Resultado(str)
+                     }
+                 }
+                 break;
+             case ".":
+                 if (str == "0" || str !== "" && !str.includes(".")) {
+                     Agregar(input)
+                 }
+                 break;
+             default:
+                 if (operacion.includes(input)) { //Si es una simbolo de operacion
+                     if (screen.textContent.length !== 0) {
+                         str += input;
+                         Reset();
+                     }
+
+                 } else {
+                     if (str !== "0") {
+                         Agregar(input)
+                     } else {
+                         if (input === "0") return
+                     }
+                 };
+                 break
+         };
+
+         console.log(str);
+
+     }
+ });
